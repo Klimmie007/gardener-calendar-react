a770abe82682fbe78a9caaaefe05c90d3a5c7f6f
Szymon Gąsiewski <szymongasiewski999@gmail.com>
2022-12-19 02:46:18 +0100

    preserve's components changes

---
+++ src/models/PreserveModel.js
@@ -0,0 +1,8 @@
+class PreserveModel {
+    constructor(name, description, productonDate, expirationDate) {
+        this.name = name;
+        this.description = description;
+        this.productonDate = productonDate;
+        this.expirationDate = expirationDate;
+    };
+}
